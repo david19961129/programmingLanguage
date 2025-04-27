d09721763816889098391d4fc8ac9a0aa3d0a572
david19961129 <david19961129@gmail.com>
2025-04-28 01:55:55 +0800

    Add files via upload

---
+++ Ch03_P57_Q2_Triangle_EX/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch03_P57_Q2_Triangle_EX
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            while ( i <= 4)
+            {
+                int j = 0;  //1
+                do          //(2)
+                {   
+                    if (j < (4 - i)) //3
+                    {
+                        Console.Write(" ");
+                    }
+                    else
+                    {
+                        Console.Write("*");
+                    }
+                    j++;    //4
+                } while (j <= 4) ;  //2
+                Console.WriteLine();
+                i++;
+            }
+            Console.ReadLine();
+        }
+    }
+}
