7d521128f8628bbbe4a7cb6dd15d167194587fad
david19961129 <david19961129@gmail.com>
2025-04-28 02:20:42 +0800

    Add files via upload

---
+++ C#/Ch03_P56_For_Loop_Q1_Triangle_EX/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch03_P56_For_Loop_Q1_Triangle_EX
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int row=0, col=0;
+            for(row=0; row<=4;row++)
+            {
+                for (col = 0; col <= row; col++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ C#/Ch03_P59_Q4_Triangle_EX/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch03_P59_Q4_Triangle_EX
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            for (int i = 0; i <= 4; i++)
+            {
+                for (int j = 0; j <= 4; j++)
+                {
+                    if(j<=(4-i))Console.Write("*");
+                
+                else
+                
+                    Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
