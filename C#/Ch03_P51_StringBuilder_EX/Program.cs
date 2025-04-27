2f171fc89207e26e79586ee084f2e89d69c409a6
david19961129 <david19961129@gmail.com>
2025-04-28 02:19:19 +0800

    Add files via upload

---
+++ C#/Ch03_P51_StringBuilder_EX/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch03_P51_StringBuilder_EX
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder sb = new StringBuilder("Hello");
+            Console.WriteLine($"sb origin:{sb}");
+
+            sb.Append(" world");
+            Console.WriteLine($"sb Append():{sb}");
+
+            sb.Remove(0, 5);
+            Console.WriteLine($"sb Remove():{sb}");
+
+            sb.Insert(0, "Bye");
+            Console.WriteLine($"sb Insert():{sb}");
+
+            string s=sb.ToString();
+            Console.WriteLine($"sb ToString():{sb}, and s:{s}");
+
+            sb.Insert(0, "Bye"); s.Insert(0, "Bye ");
+            Console.WriteLine($"sb Insert():{sb}, and s:{s}");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ C#/Ch03_P52_OneDim_Array_EX/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch03_P52_OneDim_Array_EX
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] x;
+            x = new int[3]; //1
+            x[0] = 1;
+            x[1] = 2;
+            x[2] = 3;
+            Console.WriteLine($"x[0]:{x[0]},\tx[1]:{x[1]},\tx[2]:{x[2]}");
+
+            int[] y;
+            y = new int[3] { 3, 4, 5 }; //2
+            Console.WriteLine($"y[0]:{y[0]},\ty[1]:{y[1]},\ty[2]:{y[2]}");
+
+            int[]z= { 6, 7, 8 }; //3
+            Console.WriteLine($"z[0]:{z[0]},\tz[1]:{z[1]},\tz[2]:{z[2]}");
+
+
+            Console.ReadLine();
+        }
+    }
+}
