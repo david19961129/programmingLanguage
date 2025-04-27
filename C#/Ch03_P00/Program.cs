ed9cc109f7af4debdb3fb1d0ce9599c0663871f2
david19961129 <david19961129@gmail.com>
2025-04-28 02:17:13 +0800

    Add files via upload

---
+++ C#/Ch03_P00/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch03_P00
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            short x = 10;
+            short y = 20;
+            int z = x+y;
+
+            Console.WriteLine($"x is {x},y is {y},z and {z}");
+
+            Console.WriteLine("請輸入x值");
+            x = Convert.ToInt16( Console.ReadLine() );
+            Console.WriteLine("請輸入y值");
+            y = Convert.ToInt16(Console.ReadLine());
+            z = x + y;
+            Console.WriteLine($"x is {x},y is {y},z and {z}");
+            
+            Console.ReadLine();
+        }
+    }
+}
---
+++ C#/Ch03_p44_TypeRange_EX/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch03_p44_TypeRange_EX
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            short s0, s1;
+            s0 = -32768;
+            s1 =  32767;
+            Console.WriteLine($"s0 is {s0},s1 is {s1}");
+
+            s0 = short.MinValue;
+            s1 = short.MaxValue;
+            Console.WriteLine($"short main is {s0},short max is {s1}");
+
+            int res0,res1;
+
+            res0 = (short)(s0 - 1);
+            res1 = (short)(s1 + 1);
+            Console.WriteLine($"res0 is {res0},resl is {res1}");
+
+            res0 = s0-1;
+            res1 = s1-1;
+            Console.WriteLine($"res0 is {res0},resl is {res1}");
+
+            
+            Console.ReadLine();
+        }
+    }
+}
